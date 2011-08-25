8c3e437c48c8f3724037d6bbef24f1fc0cd43250
russ <russ.guild@peopleadmin.com>
2011-08-25 09:14:53 -0500

    add all

---
+++ stanker/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stanker::Application.config.secret_token = '85dadc4771470915ec63f81ad037432f6f7fa477e4bda3eb7d0ec98750e8cc601904f712635e5af18a6ba1a920706270a9987567b1f3c6f30ce9144fe579fce5'
